5e36b51355a495094c3134dea54e9f48e4dff13e
Artur Sopov <tullers@mail.ru>
2024-08-16 14:48:37 -0400

    Created swatch

--- apptype/apptype.go
+++ apptype/apptype.go
@@ -1,6 +1,7 @@
 package apptype
 
 import (
+	"image/color"
 	"fyne.io/fyne/v2"
 )
 
@@ -11,4 +12,15 @@ type PxCanvasConfig struct {
 	CanvasOffset fyne.Position
 	PxRows, PxCols int
 	PxSize int
+}
+
+type State struct {
+	BrushColor color.Color
+	BrushType int
+	SwatchSelected int
+	FilePath string
+}
+
+func (state *State) SetFilePath(path string) {
+	state.FilePath = path
 }
